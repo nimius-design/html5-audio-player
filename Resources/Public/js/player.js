0ad6077bf04092a8d935c9a8302fd6db613df749
Johannes Hertenstein <johannes@nimius.net>
2014-01-18 17:59:51 +0100

    not depending on jQuery any more. solves #6
    – knob needs to be styled again

--- Resources/Public/js/player.js
+++ Resources/Public/js/player.js
@@ -1,304 +1,338 @@
-/*
- * Minimalistic HTML5 Audio Player - jQuery Plugin
- * @requires jQuery v1.4.2 or later
- *
- * Minimalistic HTML5 Audio Player is an html5 audio player
- *
- * Licensed under the MIT:
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright (c) 2012-, Nimius Design www.nimius-design.de)
- */
-;(function($, window, document, undefined){
+
+/**
+* HTML5audioplayer – NIMIUS
+* @param  {object} palyer the player object
+* @return {void}
+*/
+function HTML5audioplayer(inp){
+
+  this.settings = merge_options({
+    player:           document.querySelector("#audio-wrap audio"),
+    playClass:        'audio-controllerPlay',
+    pauseClass:       'audio-controllerPause',
+    nextClass:        'audio-controllerNext',
+    prevClass:       'audio-controllerPrev',
+    songSelector:     '#audio-playlist .song',
+    currentlyPlaying: '.currently-playing',
+
+    knob:             '#control .knob',
+
+    uploadFolder:     'uploads/tx_nimhtml5audioplayer/',
+  },inp);
 
 
-	// plugin start
-	$.fn.player = function(options){
 
-		// config
-		var settings = $.extend({
-			volume: 0.8,
-      // deprecated!!
-			playlistId: 'audio-playlist',
-
-      playlist:     '#audio-playlist',
-      playlistItem: '.song',
-
-      playlistOGGAttribute:   'data-ogg',
-      playlistMP3Attribute:   'data-mp3',
-      playlisttitleAttribute:   'title',
-
-      playButton:   '.audio-controllerPlay',
-      nextButton:   '.audio-controllerNext',
-      prevButton:   '.audio-controllerPrev',
-      pauseClass:    'audio-controllerPause',
-
-      currentlyPlaying:   '.currently-playing',
-
-      uploadFolder:     'uploads/tx_nimhtml5audioplayer/',
-
-		}, options);
-
-		this.each(function(){
-
-      // get HTML objects
-			var $this = $(this),
-				$miniPlayer = $this.get(0),
-				$playButton = $(settings.playButton),
-				$nextButton = $(settings.nextButton),
-				$prevButton = $(settings.prevButton),
-				$currentlyPlaying = $(settings.currentlyPlaying),
-
-        // deprecated
-				$currentSong = $($miniPlayer).find('source').attr('src');
-        // deprecated
-				$currentSongText = $($miniPlayer).find('source').data('title');
-
-      var $playlist = $(settings.playlist);
-      var $playlistItems = $playlist.find(settings.playlistItem);
+  // ===========================================
+  //                Functions
 
 
-      // ============================================================================================================
-      //               OBJECTS
 
-      /**
-       * The Song object
-       * @param  {object} inp object input in the type of {mp3: … ogg: … name: …}
-       * @return {void}
-       */
-      function song(inp){
-        if(inp.mp3)  this.mp3 = inp.mp3;
-        if(inp.ogg)  this.ogg = inp.ogg;
-        if(inp.name)  this.name = inp.name;
-        if(inp.id)  this.id = inp.id;
-        console.log(inp);
-      }
+  this.play = function(){
+    this.setLabel(this.playlist.songs[this.playlist.currentSong]);
+    this.player.play();
+    this.playButton.play();
+  }
+
+
+  /**
+  * sets the label for the song
+  * @param {song} song
+  */
+  this.setLabel = function(song){
+    // console.log(["song in setLabel",song]);
+    this.label.innerHTML = song.name;
+  }
+
+  /**
+  * actually sets the song for us
+  * @param {song} song a valid song object
+  */
+  this.setSong = function(song){
+    var out = "";
+    if(song.mp3){
+      out += '<source src="'+this.settings.uploadFolder+'/'+song.mp3+'" data-title="'+song.name+'" type="audio/mpeg" preload="auto" />'
+    }
+    if(song.ogg){
+      out += '<source src="'+this.settings.uploadFolder+'/'+song.ogg+'" data-title="'+song.name+'" type="audio/ogg" preload="auto" />'
+    }
+    console.log(this.player);
+    this.player.innerHTML = out;
+    this.setLabel(song);
+    this.playlist.currentSong = song.id;
+  }
+
+  /**
+  * i guess you can guess what this does
+  * @return {void}
+  */
+  this.next = function(){
+    var playing = !this.player.paused;
+    this.setSong(this.playlist.next());
+    this.player.load();
+    if(playing){this.play();}
+  }
+
+  /**
+  * i guess you can guess what this does
+  * @return {void}
+  */
+  this.prev = function(){
+    var playing = !this.player.paused;
+    this.setSong(this.playlist.prev());
+    this.player.load();
+    if(playing){this.play();}
+  }
+
+
+  this.pause = function(){
+    this.player.pause();
+    this.playButton.pause();
+  }
+
+  this.toggleplay = function(){
+    if(this.player.paused){
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
+
+  this.setVolume = function(vol){
+    this.player.volume = vol;
+  }
+  //
+  // ===============================================
+  //                initialization
+
+  HTML5audioplayer = this;
+
+  // define the player for everyone
+  this.player = (typeof(this.settings.player) == "object") ? this.settings.player : document.querySelector(this.settings.player);
+
+  // the label for everyone
+  this.label = (typeof(this.settings.currentlyPlaying) == "object") ? this.settings.currentlyPlaying : document.querySelector(this.settings.currentlyPlaying);
+
+  // initialize the play button
+  this.playButton = new playButton({
+    button: '.'+this.settings.playClass,
+    play:   this.settings.playClass,
+    pause:  this.settings.pauseClass
+  });
+  // this.knob = new knob();
+  this.playlist = new playlist(this.settings.songSelector);
+
+  // set the first song
+  this.setSong(this.playlist.songs[1]);
+
+  this.nextButton = document.querySelector('.' + this.settings.nextClass);
+  this.prevButton = document.querySelector('.' + this.settings.prevClass);
+
+  this.knob = new jimKnopf({
+    knob:   this.settings.knob,
+  })
+
+
+
+  // ================================================
+  //                  click handler
+
+  this.playButton.button.onclick = function(){
+    HTML5audioplayer.toggleplay();
+  }
+
+  this.nextButton.onclick = function(){
+    HTML5audioplayer.next();
+  }
+
+  this.prevButton.onclick = function(){
+    HTML5audioplayer.prev();
+  }
+
+  document.querySelector(this.settings.knob).onchange = function(){
+        HTML5audioplayer.setVolume(this.value);
+
+  };
+
+
+
+
+
+
+  // ==============================================
+  //
+  /**
+   * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
+   * http://stackoverflow.com/questions/171251/how-can-i-merge-properties-of-two-javascript-objects-dynamically
+   *
+   * @param obj1
+   * @param obj2
+   * @returns obj3 a new object based on obj1 and obj2
+   */
+   function merge_options(obj1,obj2){
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+  }
+
+
+  /**
+  * The Song object
+  * @param  {object} inp object input in the type of {mp3: … ogg: … name: …}
+  * @return {void}
+  */
+  function song(inp){
+    if(inp.mp3)  this.mp3 = inp.mp3;
+    if(inp.ogg)  this.ogg = inp.ogg;
+    if(inp.name)  this.name = inp.name;
+    if(inp.id)  this.id = inp.id;
+    console.log(inp);
+  }
+
+
+  // ------------------------------------------------------------------
+
+  // function knob(){
+  //   $('#control').knobKnob({
+  //     snap : 10,
+  //     value: 359 * player.player.volume,
+  //     turn : function(ratio){
+  //       player.setVolume(ratio);
+  //     }
+  //   });
+  // }
+
+  // ------------------------------------------------------------------
+
+  function playButton(inp){
+    this.button = (typeof(inp.button) == "object") ? inp.button : document.querySelector(inp.button);
+    this.playClass = inp.play;
+    this.pauseClass = inp.pause;
+    this.playing;
+
+    this.pause = function(){
+      this.button.setAttribute("class",this.playClass);
+      this.playing = false;
+    }
+
+    this.play  = function(){
+      this.button.setAttribute("class",this.pauseClass);
+      this.playing = true;
+    }
+  }
 
 
       // ------------------------------------------------------------------
 
-      function knob(){
-        $('#control').knobKnob({
-          snap : 10,
-          value: 359 * player.player.volume,
-          turn : function(ratio){
-            player.setVolume(ratio);
-          }
-        });
+  /**
+  * Playlist object
+  * @return {void}
+  */
+  function playlist(songSelector){
+    playlist = this;
+    console.log(songSelector);
+
+    this.currentSong = 1;
+    // build the actual playlist
+    this.songs = [{},];
+
+    var s = document.querySelectorAll(songSelector);
+    for(var i=0; i<s.length; i++){
+
+      var id = i+1;
+
+      this.songs.push(new song({
+        name:   s[i].getAttribute("data-name") ? s[i].getAttribute("data-name") : false,
+        mp3:    s[i].getAttribute("data-mp3")  ? s[i].getAttribute("data-mp3")  : false,
+        ogg:    s[i].getAttribute("data-ogg")  ? s[i].getAttribute("data-ogg")  : false,
+        id:     id,
+      }));
+      s[i].setAttribute("data-id",id);
+
+      s[i].onclick = function(){
+        console.log(this)
+        HTML5audioplayer.setSong(playlist.songs[this.getAttribute("data-id")]);
+        HTML5audioplayer.player.load();
+        HTML5audioplayer.play();
+      }
+    }
+
+
+    /**
+    * next function: increases current counter and returns the new song object
+    * @return {song}
+    */
+    this.next = function(){
+      var nextSong;
+      if(this.currentSong+1 < this.songs.length){
+        nextSong = this.currentSong+1;
+      } else {
+        nextSong = 1;
       }
 
-      // ------------------------------------------------------------------
+      this.currentSong = nextSong;
+      return this.songs[nextSong];
+    }
 
-      function playButton(){
-        this.button = $(settings.playButton);
-        this.playing;
+    /**
+    * prev function: decreases the current counter and returns the new song object
+    * @return {song}
+    */
+    this.prev = function(){
+      var prevSong;
+      if(this.currentSong-1 >= 1){
+        prevSong = this.currentSong-1;
+      } else {
+        prevSong = this.songs.length-1;
       }
 
-      playButton.prototype.pause = function(){
-        this.button.removeClass(settings.pauseClass);
-        this.button.addClass(settings.playButton);
-        this.playing = false;
-      }
-
-      playButton.prototype.play  = function(){
-        this.button.addClass(settings.pauseClass);
-        this.button.removeClass(settings.playButton);
-        this.playing = true;
-      }
-
-      // ------------------------------------------------------------------
-
-      /**
-       * Playlist object
-       * @return {void}
-       */
-      function playlist(){
-        playlist = this;
-
-        // for some weird reason we have to start at 1 here
-        this.currentSong = 1;
-        // build the actual playlist
-        this.songs = [{},];
-        $playlistItems.each(function(n,v){
-          console.log([typeof(n),n]);
-          var id = n+1;
-          playlist.songs.push(new song({
-            name: $(this).attr(settings.playlisttitleAttribute) ? $(this).attr(settings.playlisttitleAttribute) : false,
-            mp3:  $(this).attr(settings.playlistMP3Attribute) ? $(this).attr(settings.playlistMP3Attribute) : false,
-            ogg:  $(this).attr(settings.playlistOGGAttribute) ? $(this).attr(settings.playlistOGGAttribute) : false,
-            id:   parseInt(id),
-          }));
-          $(this).click(function(){
-            player.setSong(playlist.songs[id]);
-            player.player.load();
-            player.play();
-          });
-        });
-
-        console.log(playlist);
-      }
-
-      /**
-       * next function: increases current counter and returns the new song object
-       * @return {song}
-       */
-      playlist.prototype.next = function(){
-        var nextSong;
-        if(this.currentSong+1 < this.songs.length){
-          nextSong = this.currentSong+1;
-        } else {
-          nextSong = 1;
-        }
-
-        this.currentSong = nextSong;
-        return this.songs[nextSong];
-      }
-
-      /**
-       * prev function: decreases the current counter and returns the new song object
-       * @return {song}
-       */
-      playlist.prototype.prev = function(){
-        var prevSong;
-        if(this.currentSong-1 >= 1){
-          prevSong = this.currentSong-1;
-        } else {
-          prevSong = this.songs.length-1;
-        }
-
-        this.currentSong = prevSong;
-        console.log(["nextSong in playlist.prev d()",prevSong]);
-        return this.songs[prevSong];
-      }
+      this.currentSong = prevSong;
+      console.log(["nextSong in playlist.prev d()",prevSong]);
+      return this.songs[prevSong];
+    }
+  }
 
 
-      // ------------------------------------------------------------------
-
-      /**
-       * the player object
-       * @param  {object} palyer the player object
-       * @return {void}
-       */
-      function player(player){
-        this.player = player;
-        this.playButton = new playButton();
-        // this.knob = new knob();
-        this.playlist = new playlist();
-      }
-
-      /**
-       * sets the label for the song
-       * @param {song} song
-       */
-      player.prototype.setLabel = function(song){
-        console.log(["song in setLabel",song]);
-        $currentlyPlaying.html(song.name);
-      }
-
-      /**
-       * actually sets the song for us
-       * @param {song} song a valid song object
-       */
-      player.prototype.setSong = function(song){
-        var out = "";
-        if(song.mp3){
-          out += '<source src="'+settings.uploadFolder+'/'+song.mp3+'" data-title="'+song.name+'" type="audio/mpeg" preload="auto" />'
-        }
-        if(song.ogg){
-          out += '<source src="'+settings.uploadFolder+'/'+song.ogg+'" data-title="'+song.name+'" type="audio/ogg" preload="auto" />'
-        }
-        console.log(this.player);
-        this.player.innerHTML = out;
-        this.setLabel(song);
-        this.playlist.currentSong = song.id;
-      }
-
-      /**
-       * i guess you can guess what this does
-       * @return {void}
-       */
-      player.prototype.next = function(){
-        var playing = !this.player.paused;
-        this.setSong(this.playlist.next());
-        this.player.load();
-        if(playing){this.play();}
-      }
-
-      /**
-       * i guess you can guess what this does
-       * @return {void}
-       */
-      player.prototype.prev = function(){
-        var playing = !this.player.paused;
-        this.setSong(this.playlist.prev());
-        this.player.load();
-        if(playing){this.play();}
-      }
-
-      player.prototype.play = function(){
-        this.setLabel(this.playlist.songs[this.playlist.currentSong]);
-        this.player.play();
-        this.playButton.play();
-      }
-
-      player.prototype.pause = function(){
-        this.player.pause();
-        this.playButton.pause();
-      }
-
-      player.prototype.toggleplay = function(){
-        if(this.player.paused){
-          this.play();
-        } else {
-          this.pause();
-        }
-      }
-
-      player.prototype.setVolume = function(vol){
-        this.player.volume = vol;
-      }
-
-
-
-
-      player = new player($this.get(0));
-      player.knob = new knob();
-
-      player.setSong(player.playlist.songs[player.playlist.currentSong]);
-
-
-      // =====================================================================
-      //                          // EVENT HANDLERS
-
-      $(settings.playButton).click(function(){
-        player.play();
-      });
-
-      $(settings.pauseClass).click(function(){
-        player.pause();
-      });
-
-      $(settings.nextButton).click(function(){
-        player.next();
-      })
-
-      $(settings.prevButton).click(function(){
-        player.prev();
-      });
-
-		});
-
-		// returns the object
-		return this;
-
-	// plugin end
-	};
-
-})(jQuery,window,document);
+  function jimKnopf(inp){
+    this.element = (typeof(inp.knob) == "object") ? inp.knob : document.querySelector(inp.knob);
 
 
 
 
 
+    P1 = function() {
+
+    };
+
+    P1.prototype = Object.create(Ui.prototype);
+
+    P1.prototype.createElement = function() {
+      "use strict";
+      Ui.prototype.createElement.apply(this, arguments);
+      this.addComponent(new Ui.Pointer({
+        type: 'Rect',
+        pointerWidth: 3,
+        pointerHeight: this.width / 5,
+        offset: this.width / 2 - this.width / 3.3 - this.width / 10
+      }));
+
+      // this.addComponent(new Ui.Scale(this.merge(this.options, {
+      //   drawScale: false,
+      //   drawDial: true,
+      //   radius: this.width/2.6})));
+
+      var circle = new Ui.El.Circle(this.width / 3.3, this.width / 2, this.height / 2);
+      this.el.node.appendChild(circle.node);
+      this.el.node.setAttribute("class", "p1");
+};
+
+
+
+        this.jim = new Knob(this.element, new P1());
+        // window.jim = this.jim;
+
+  }
+
+}
 
 
