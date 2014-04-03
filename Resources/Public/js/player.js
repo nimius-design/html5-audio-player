7d8e1a78ed806ac4e9ff4f9a731ccb970107a733
Johannes Hertenstein <johannes@nimius.net>
2014-04-03 10:41:08 +0200

    Cleanup

--- Resources/Public/js/player.js
+++ Resources/Public/js/player.js
@@ -16,24 +16,102 @@ function HTML5audioplayer(inp) {
         knob: '#control .knob',
         time: '#control .time',
 
-        uploadFolder: 'uploads/tx_nimhtml5audioplayer/',
+        uploadFolder: 'uploads/tx_nimhtml5audioplayer/'
     }, inp);
 
 
     // ===========================================
     //                Functions
 
+    // Controlling functions, use these to control the player
 
+    /**
+     * starts the playback
+     */
     this.play = function () {
         this.setLabel(this.playlist.songs[this.playlist.currentSong]);
         this.player.play();
         this.playButton.play();
     }
 
+    /**
+     * pauses the playback
+     */
+    this.pause = function () {
+        this.player.pause();
+        this.playButton.pause();
+    }
+
+    /**
+     * toggles the play / pause state
+     */
+    this.toggleplay = function () {
+        if (this.player.paused) {
+            this.play();
+        } else {
+            this.pause();
+        }
+    }
+
+    /**
+     * i guess you can guess what this does
+     */
+    this.next = function () {
+        // see if the player was playing before
+        var playing = !this.player.paused;
+        this.setSong(this.playlist.next());
+        this.player.load();
+
+        // if the player was playing before, then start playing again
+        if (playing) {
+            this.play();
+        }
+    }
+
+    /**
+     * i guess you can guess what this does
+     */
+    this.prev = function () {
+        // see if the player was playing before
+        var playing = !this.player.paused;
+        this.setSong(this.playlist.prev());
+        this.player.load();
+
+        // if the player was playing before, then start playing again
+        if (playing) {
+            this.play();
+        }
+    }
+
+    /**
+     * skip to a certain time in the song
+     * @param {number} time time in the song in seconds (must be lower than the total length of the song)
+     */
+    this.setTime = function (time) {
+        if (time < this.player.seekable.end(0)) {
+            this.player.currentTime = time;
+        }
+    }
+
+    /**
+     * sets the Volume
+     * @param {number} vol the volume, between 0.0 (mute) and 1.0 (full)
+     */
+    this.setVolume = function (vol) {
+        this.player.volume = vol;
+
+        if (vol > 0 && vol < 1) {
+            //console.log(this);
+            this.knob.update(vol);
+        }
+    }
+
+
+    // internal functions, unless you know what you are doing, don't use them from outwards
 
     /**
      * sets the label for the song
-     * @param {song} song
+     * @param {HTML5audioplayer.song} song
      */
     this.setLabel = function (song) {
         var songs = document.querySelectorAll(this.settings.songSelector);
@@ -45,7 +123,7 @@ function HTML5audioplayer(inp) {
 
     /**
      * actually sets the song for us
-     * @param {song} song a valid song object
+     * @param {HTML5audioplayer.song} song a valid song object
      */
     this.setSong = function (song) {
         var out = "";
@@ -62,89 +140,34 @@ function HTML5audioplayer(inp) {
     }
 
     /**
-     * i guess you can guess what this does
-     * @return {void}
+     * gets the progress of the song
+     * @param {boolean} convertToPercent should be true, if you want percentages (0%-100%), if false it returns floats (0.0 - 1.0)
+     * @returns {number}
      */
-    this.next = function () {
-        var playing = !this.player.paused;
-        this.setSong(this.playlist.next());
-        this.player.load();
-        if (playing) {
-            this.play();
+    this.getPercentage = function (convertToPercent) {
+        var time =  this.player.currentTime / this.player.seekable.end(0);
+        if(convertToPercent){
+            time *= 100;
         }
+        return time;
     }
 
     /**
-     * i guess you can guess what this does
-     * @return {void}
+     * sets the rimerunner to the current position in the song
      */
-    this.prev = function () {
-        var playing = !this.player.paused;
-        this.setSong(this.playlist.prev());
-        this.player.load();
-        if (playing) {
-            this.play();
-        }
-    }
-
-
-    this.pause = function () {
-        this.player.pause();
-        this.playButton.pause();
-    }
-
-    this.toggleplay = function () {
-        if (this.player.paused) {
-            this.play();
-        } else {
-            this.pause();
-        }
-    }
-
-    this.setVolume = function (vol) {
-        this.player.volume = vol;
-
-        if (vol > 0 && vol < 1) {
-            //console.log(this);
-            this.knob.update(vol);
-        }
-    }
-
-    this.returnSong = function (inp) {
-        return new song(inp);
-    }
-
-
-    //
-    //
-    //
-
-    this.setTime = function (time) {
-        if (time < this.player.seekable.end(0)) {
-            //console.log(time);
-            this.player.currentTime = time;
-            //this.time.update(time);
-        }
-    }
-
-    this.getPercentage = function () {
-        return this.player.currentTime / this.player.seekable.end(0);
-    }
-
-    this.time = new jimKnopf({
-        knob: this.settings.time,
-        thickness: 50,
-    });
-
     this.updateTime = function () {
         if (!this.player.paused && this.getPercentage() != 0) {
-            this.time.update(
+            this.timerunner.update(
                 this.getPercentage()
             );
         }
         //console.log(this);
     }
 
+    /**
+     * binds the updateTime function to an interval every second
+     * @param {boolean} bind if true it sets the interval, if false it removes / clears it
+     */
     this.bindTimeUpdate = function (bind) {
         if (bind) {
             this.timeUpdateInterval = setInterval(this.updateTime.bind(this), 100);
@@ -153,80 +176,6 @@ function HTML5audioplayer(inp) {
         }
     }
 
-    this.bindTimeUpdate(true);
-
-
-    //
-    // ===============================================
-    //                initialization
-
-    HTML5audioplayer = this;
-
-    // define the player for everyone
-    this.player = (typeof(this.settings.player) == "object") ? this.settings.player : document.querySelector(this.settings.player);
-
-    // the label for everyone
-    // this.label = (typeof(this.settings.currentlyPlaying) == "object") ? this.settings.currentlyPlaying : document.querySelector(this.settings.currentlyPlaying);
-
-    // initialize the play button
-    this.playButton = new playButton({
-        button: '.' + this.settings.playClass,
-        play: this.settings.playClass,
-        pause: this.settings.pauseClass
-    });
-    // this.knob = new knob();
-    this.playlist = new playlist(this.settings.songSelector);
-    // set the first song
-    this.setSong(this.playlist.songs[1]);
-
-    this.nextButton = document.querySelector('.' + this.settings.nextClass);
-    this.prevButton = document.querySelector('.' + this.settings.prevClass);
-
-    this.knob = new jimKnopf({
-        knob: this.settings.knob,
-    })
-
-    this.setVolume(0.999);
-
-
-    // ================================================
-    //                  click handler
-
-    this.playButton.button.onclick = function () {
-        HTML5audioplayer.toggleplay();
-    }
-
-    this.nextButton.onclick = function () {
-        HTML5audioplayer.next();
-    }
-
-    this.prevButton.onclick = function () {
-        HTML5audioplayer.prev();
-    }
-
-    document.querySelector(this.settings.knob).onchange = function () {
-        HTML5audioplayer.setVolume(this.value);
-
-    };
-
-    this.time.element.onchange = function () {
-        HTML5audioplayer.bindTimeUpdate(false);
-        HTML5audioplayer.pause();
-        HTML5audioplayer.setTime(this.value * HTML5audioplayer.player.seekable.end(0));
-        setTimeout(function () {
-            HTML5audioplayer.play();
-        }, 1000);
-        HTML5audioplayer.bindTimeUpdate(true);
-    }
-
-    this.player.onended = function(){
-        HTML5audioplayer.next();
-        HTML5audioplayer.play();
-    }
-
-
-    // ==============================================
-    //
     /**
      * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
      * http://stackoverflow.com/questions/171251/how-can-i-merge-properties-of-two-javascript-objects-dynamically
@@ -247,6 +196,105 @@ function HTML5audioplayer(inp) {
     }
 
 
+    //
+    // ===============================================
+    //                initialization
+
+    // for easier use later on and no scope confusion in the handlers
+    HTML5audioplayer = this;
+
+    // sets the player object / element
+    // you can input both: a already selected audio element or a selector
+    this.player = (typeof(this.settings.player) == "object") ? this.settings.player : document.querySelector(this.settings.player);
+
+    // initialize the play button
+    this.playButton = new playButton({
+        button: '.' + this.settings.playClass,
+        play:   this.settings.playClass,
+        pause:  this.settings.pauseClass
+    });
+
+    // initialize the playlist object
+    this.playlist = new playlist(this.settings.songSelector);
+
+    // set the first song note here: this array starts at 1, cause starting at 0 causes trouble
+    this.setSong(this.playlist.songs[1]);
+
+    // sets the next and prev button
+    this.nextButton = document.querySelector('.' + this.settings.nextClass);
+    this.prevButton = document.querySelector('.' + this.settings.prevClass);
+
+    // initializes the volume knob
+    this.knob = new jimKnopf({
+        knob: this.settings.knob
+    });
+
+    // sets the volume to full volume by default
+    this.setVolume(0.999);
+
+    // initialize the timerunner
+    this.timerunner = new jimKnopf({
+        knob: this.settings.time,
+        thickness: 50
+    });
+
+    // bind the update
+    this.bindTimeUpdate(true);
+
+
+
+
+
+
+    // ================================================
+    //                  event handler
+
+    this.playButton.button.onclick = function () {
+        HTML5audioplayer.toggleplay();
+    }
+
+    this.nextButton.onclick = function () {
+        HTML5audioplayer.next();
+    }
+
+    this.prevButton.onclick = function () {
+        HTML5audioplayer.prev();
+    }
+
+    this.knob.element.onchange = function () {
+        HTML5audioplayer.setVolume(this.value);
+    };
+
+    this.timerunner.element.onchange = function () {
+        // first unbind the automatic update every second to avoid the control "jumping around" and pausing the player for the same reason
+        HTML5audioplayer.bindTimeUpdate(false);
+        HTML5audioplayer.pause();
+
+        // set the actual value (the input ranges from 0.0 to 1.0, so we have to multiply by the total length of the song to get the seconds)
+        HTML5audioplayer.setTime(this.value * HTML5audioplayer.player.seekable.end(0));
+
+        // start playing again after 1second
+        setTimeout(function () {
+            HTML5audioplayer.play();
+        }, 1000);
+
+        // bind the timerunner update again
+        HTML5audioplayer.bindTimeUpdate(true);
+    }
+
+    this.player.onended = function(){
+        HTML5audioplayer.next();
+        HTML5audioplayer.play();
+    }
+
+
+
+
+
+    // ==============================================
+    //                  Objects
+
+
     /**
      * The Song object
      * @param  {object} inp object input in the type of {mp3: … ogg: … name: …}
@@ -260,21 +308,10 @@ function HTML5audioplayer(inp) {
         //console.log(inp);
     }
 
-
-    // ------------------------------------------------------------------
-
-    // function knob(){
-    //   $('#control').knobKnob({
-    //     snap : 10,
-    //     value: 359 * player.player.volume,
-    //     turn : function(ratio){
-    //       player.setVolume(ratio);
-    //     }
-    //   });
-    // }
-
-    // ------------------------------------------------------------------
-
+    /**
+     * the play button
+     * @param {object} inp
+     */
     function playButton(inp) {
         this.button = (typeof(inp.button) == "object") ? inp.button : document.querySelector(inp.button);
         this.playClass = inp.play;
@@ -293,20 +330,17 @@ function HTML5audioplayer(inp) {
     }
 
 
-    // ------------------------------------------------------------------
-
     /**
      * Playlist object
      * @return {void}
      */
     function playlist(songSelector) {
         playlist = this;
-        //console.log(songSelector);
 
         this.currentSong = 1;
         // build the actual playlist
         this.songs = [
-            {},
+            {}
         ];
 
         var s = document.querySelectorAll(songSelector);
@@ -333,7 +367,7 @@ function HTML5audioplayer(inp) {
 
         /**
          * next function: increases current counter and returns the new song object
-         * @return {song}
+         * @return {HTML5audioplayer.song}
          */
         this.next = function () {
             var nextSong;
@@ -349,7 +383,7 @@ function HTML5audioplayer(inp) {
 
         /**
          * prev function: decreases the current counter and returns the new song object
-         * @return {song}
+         * @return {HTML5audioplayer.song}
          */
         this.prev = function () {
             var prevSong;
@@ -366,6 +400,10 @@ function HTML5audioplayer(inp) {
     }
 
 
+    /**
+     * a wrapper for the turning knob library jim knopf
+     * @param inp
+     */
     function jimKnopf(inp) {
 
         /*
@@ -373,16 +411,17 @@ function HTML5audioplayer(inp) {
          */
         this.element = (typeof(inp.knob) == "object") ? inp.knob : document.querySelector(inp.knob);
 
+        /**
+         * default for thickness
+         */
         if (!inp.thickness) {
             inp.thickness = 15;
         }
 
-        var P2 = function () {
-        };
-
-        P3 = function() {
-        };
-
+        /**
+         * as taken from one of the examples on the site
+         */
+        P3 = function() {};
         P3.prototype = Object.create(Ui.prototype);
 
         P3.prototype.createElement = function() {
@@ -398,13 +437,12 @@ function HTML5audioplayer(inp) {
 
 
         this.jim = new Knob(this.element, new P3());
-        // window.jim = this.jim;
 
-
-        /*
-         ------------------
+        /**
+         * update the state of the knob
+         * @param {number} num either a float between 0.0 - 1.0 or percentage between 0% - 100%
+         * @param {boolean} isPercent if true, it indicates, that the given value is to treat as a percentage
          */
-
         this.update = function (num, isPercent) {
             if (typeof isPercent !== "undefined" || isPercent) {
                 num /= 100;
